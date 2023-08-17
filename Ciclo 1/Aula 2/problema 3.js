271f7dedf9834cc31de3857416129c32606a20a0
ThiagoPeloso <thiago02peloso@gmail.com>
2023-08-16 21:36:49 -0300

    final aula 2

--- Ciclo 1/Aula 2/problema 3.js
+++ Ciclo 1/Aula 2/problema 3.js
@@ -1,6 +1,6 @@
 var custoFabrica = 10000
 var distribuidor = custoFabrica * 28 / 100
 var impostos = custoFabrica * 45 / 100
-var custoFinal = custoFabrica + (distribuidor + impostos);
+var custoFinal = custoFabrica + distribuidor + impostos;
 
 console.log(`O custo de fábrica é de ${custoFabrica}.\nConsiderando que o valor do percentual do distribuidor é de ${distribuidor},\ne que o valor do percentual dos impostos é de ${impostos},\npodemos concluir que o valor final do carro é de ${custoFinal}.`);
